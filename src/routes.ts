71514fd3cb73537f4a1f0365438f29da2641305f
Emerson Mendes <devmendesc@gmail.com>
2023-05-12 18:55:48 -0300

    seleção de entregas pela deliveryman e exclusão do retorno de entregas já selecionadas

--- src/routes.ts
+++ src/routes.ts
@@ -7,6 +7,7 @@ import { CreateDeliveryController } from "./models/deliveries/useCase/createDeli
 import { ensureAuthenticateClient } from "./middlewares/ensureAuthenticateClient";
 import { FindAllAvailableController } from "./models/deliveries/useCase/findAllAvailable/FindAllAvailableController";
 import { ensureAuthenticateDeliveryman } from "./middlewares/ensureAuthenticateDeliveryman";
+import { UpdateDeliverymanController } from "./models/deliveries/useCase/updateDelivaryman/useCases/UpdateDeliverymanController";
 
 export const routes = Router();
 
@@ -16,6 +17,7 @@ const createDeliverymanController = new CreateDeliverymanController()
 const authenticateDeliverymanController = new AuthenticateDeliverymanController()
 const createDeliveryController = new CreateDeliveryController()
 const findAllAvailableController = new FindAllAvailableController()
+const updateDeliverymanController = new UpdateDeliverymanController()
 
 //Login 
 routes.post("/login/client", authenticateClienteController.handle)
@@ -25,6 +27,9 @@ routes.post("/login/deliveryman", authenticateDeliverymanController.handle)
 routes.post("/clients", createClientController.handle);
 routes.post("/deliveryman", createDeliverymanController.handle)
 routes.post("/delivery", ensureAuthenticateClient, createDeliveryController.handle)
+
+//Atualização
+routes.put("/delivery/updateDeliveryman/:id", ensureAuthenticateDeliveryman, updateDeliverymanController.handle)
 
 //buscar
 routes.get("/delivery/available", ensureAuthenticateDeliveryman,findAllAvailableController.handle)

7bbefd205cf1da698d2c304cdc2f0b5a270e2f46
Emerson Mendes <devmendesc@gmail.com>
2023-05-12 17:47:32 -0300

    Criação de verificação de token e delivery

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,8 @@ import { CreateClientController } from "./models/clients/useCases/createClient/c
 import { AuthenticateClientController } from "./models/account/authenticateClient/AUthenticateClientController";
 import { CreateDeliverymanController } from "./models/deliveryman/useCase/createDEliveryman/createDeliveryController";
 import { AuthenticateDeliverymanController } from "./models/account/authenticateDeliveryman/AuthenticateDEliverymanController";
+import { CreateDeliveryController } from "./models/deliveries/useCase/createDelivery/CreateDeliveryController";
+import { ensureAuthenticateClient } from "./middlewares/ensureAuthenticateClient";
 
 export const routes = Router();
 
@@ -10,6 +12,7 @@ const createClientController = new CreateClientController();
 const authenticateClienteController = new AuthenticateClientController()
 const createDeliverymanController = new CreateDeliverymanController()
 const authenticateDeliverymanController = new AuthenticateDeliverymanController()
+const createDeliveryController = new CreateDeliveryController()
 
 
 routes.post("/login/client", authenticateClienteController.handle)
@@ -17,3 +20,4 @@ routes.post("/login/deliveryman", authenticateDeliverymanController.handle)
 
 routes.post("/clients", createClientController.handle);
 routes.post("/deliveryman", createDeliverymanController.handle)
+routes.post("/delivery", ensureAuthenticateClient, createDeliveryController.handle)

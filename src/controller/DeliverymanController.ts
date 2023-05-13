3320abb655da3265ece93c4b3f26297643cd3352
Emerson Mendes <devmendesc@gmail.com>
2023-05-13 20:24:15 -0300

    Criação das classes DeliverymanService e DelyverymanController

---
+++ src/controller/DeliverymanController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { DeliverymanService } from "../services/DeliverymanService";
+
+const deliverymanService = new DeliverymanService();
+
+export class ClientController {
+  async createDeliveryman(request: Request, response: Response) {
+    const { username, password } = request.body;
+    const newDeliveryman = await deliverymanService.createDeliveryman({ password, username });
+    return response.status(201).json(newDeliveryman);
+  }
+
+  async findDeliverymanById(request: Request, response: Response) {
+    const { id_deliveryman } = request;
+    const deliveryman = await deliverymanService.findDeliverymanById(id_deliveryman);
+    return response.status(200).json(deliveryman);
+  }
+
+  async findAllDeliveries(request: Request, response: Response) {
+    const { id_deliveryman } = request;
+    const deliveries = await deliverymanService.findAllDeliveries(id_deliveryman);
+    return response.status(200).json(deliveries);
+  }
+}

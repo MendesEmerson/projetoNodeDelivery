1c2f1eccd07bd8c300ee0e69f5dba98c508d8b45
juancassiano <juancassiano@hotmail.com>
2023-05-28 02:41:30 -0300

    :BUG: correção de bug sobre retorno de tratamento

--- src/middlewares/ensureAuthenticateClient.ts
+++ src/middlewares/ensureAuthenticateClient.ts
@@ -1,28 +1,31 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
+import { TokenMissingException } from "../services/exceptionsHandler/genericsExceptions/TokenMissingException";
+import { InvalidTokenException } from "../services/exceptionsHandler/genericsExceptions/InvalidTokenException";
 
 interface IPayload {
   sub: string;
 }
 
-export async function ensureAuthenticateClient(req: Request, res: Response, next: NextFunction) {
-  const authHeader = req.headers.authorization;
+export async function ensureAuthenticateClient(request: Request, response: Response, next: NextFunction) {
+  try {
+  
+  const authHeader = request.headers.authorization;
 
   if (!authHeader) {
-    return res.status(401).json({
-      error: "Token missing",
-    });
+    throw new TokenMissingException()
   }
 
   const [, token] = authHeader.split(" ");
 
-  try {
     const { sub } = verify(token, "bbca61cf2f25de7dfbe347b803122fda") as IPayload;
-    req.id_client = sub
+    request.id_client = sub
     return next()
   } catch (error) {
-    return res.status(401).json({
-      error: "Invalid token",
-    });
+    if (error instanceof TokenMissingException) {
+      return response.status(error.status).json(error)
+    }
+      const invalidToken = new InvalidTokenException()
+      return response.status(invalidToken.status).json(invalidToken)
   }
 }
--- src/middlewares/ensureAuthenticateDeliveryman.ts
+++ src/middlewares/ensureAuthenticateDeliveryman.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response, response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 import { TokenMissingException } from "../services/exceptionsHandler/genericsExceptions/TokenMissingException";
 import { InvalidTokenException } from "../services/exceptionsHandler/genericsExceptions/InvalidTokenException";
@@ -7,24 +7,24 @@ interface IPayload {
   sub: string;
 }
 
-export async function ensureAuthenticateDeliveryman(req: Request, res: Response, next: NextFunction) {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader) {
-    throw new TokenMissingException()
-  }
-
-  const [, token] = authHeader.split(" ");
+export async function ensureAuthenticateDeliveryman(request: Request, response: Response, next: NextFunction) {
 
   try {
+  const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      throw new TokenMissingException()
+    }
+  
+    const [, token] = authHeader.split(" ");
     const { sub } = verify(token, "bbca61cf2f17de7dfbe347b803122fda") as IPayload;
-    req.id_deliveryman = sub
+    request.id_deliveryman = sub
     return next()
   } catch (error) {
     if (error instanceof TokenMissingException) {
       return response.status(error.status).json(error)
     }
-    const invalidToken = new InvalidTokenException()
-    return response.status(invalidToken.status).json(invalidToken)
+      const invalidToken = new InvalidTokenException()
+      return response.status(invalidToken.status).json(invalidToken)
   }
 }
